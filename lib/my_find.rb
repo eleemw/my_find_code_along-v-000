54ebd770de754086cdeda5be58b6559b22d87aab
Erica Murdock-Waters <ericahpa@gmail.com>
2018-03-07 14:25:40 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,11 +4,11 @@ def my_find(collection)
   i = 0
 
   while i < collection.length
-    if yield(collection[i])
-      return collection[i]
-    end
+  #  if yield(collection[i])
+  #    return collection[i]
+  #  end
+  # can refactor if statement all on one line using a statement modifier
+    return collection[i] if yield(collection[i])
     i += 1
-
   end
-
 end
